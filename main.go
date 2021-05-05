7f0b3b68448c1c02c1628c7ac6821cfe78aee231
AndrewSich <andrewsetyawan123@gmail.com>
2021-05-05 22:50:38 +0700

    FIX: Setup enviroment

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,11 +12,24 @@ func main() {
 	http.HandleFunc("/", handlerIndex)
 
 	// run server
-	log.Printf("Server Running on Port 8080\n")
-	err := http.ListenAndServe(":80", nil)
+	log.Printf("Server Running On\n")
+	err := http.ListenAndServe(GetPort(), nil)
 	if err != nil {
 		log.Println(err.Error())
 	}
+}
+
+// Get Port
+func GetPort() string {
+	var port = os.Getenv("PORT")
+
+	// get default port if port not found
+	if port == "" {
+		port = "4747"
+		log.Println("[!] No PORT env variable detect, default to " + port)
+	}
+
+	return ":" + port
 }
 
 // Handler Index Page
